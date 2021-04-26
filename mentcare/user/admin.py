40521f9113dff904f5779b7e5d704768c52b09ce
danyalahmadieh <82466941+danyalahmadieh@users.noreply.github.com>
2021-04-26 23:43:23 +0300

    Add files via upload

--- mentcare/user/admin.py
+++ mentcare/user/admin.py
@@ -4,21 +4,4 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.admin import SimpleListFilter
 
 
-# Register your models here.
-@admin.register(CustomUser)
-class addUser(UserAdmin):
-    list_filter = ("role", )
-    fieldsets = (
-        *UserAdmin.fieldsets,  # original form fieldsets, expanded
-        (                      # new fieldset added on to the bottom
-            'Role',  # group heading of your choice; set to None for a blank space instead of a header
-            {
-                'fields': (
-                    'role',
-                ),
-            },
-        ),
-    )
-    ordering = ('email',)
-
-
+admin.site.register(CustomUser)
--- mentcare/user/models.py
+++ mentcare/user/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
 from django.utils.translation import gettext_lazy as _
-
+from .managers import UserManager
 # Create your models here.
 class CustomUser(AbstractUser):
     id=models.UUIDField( primary_key = True, editable = False,default=uuid.uuid4()) 
     role=models.CharField(max_length=10,choices=[('Clinic', 'Clinic'), ('Patient', 'Patient')])
-    username = None
     is_active=models.BooleanField(default=True)
+    username=None
     USERNAME_FIELD = 'email'
     email = models.EmailField(_('email address'), unique=True) # changes email to unique and blank to false
+    objects = UserManager() 
     REQUIRED_FIELDS = [] # removes email from REQUIRED_FIELDS
     class Meta:
         db_table='User'
         verbose_name_plural='Users'
-        
