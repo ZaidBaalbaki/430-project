48f56b169851ce64984b5fb17369f64ea1625c3c
danyalahmadieh <82466941+danyalahmadieh@users.noreply.github.com>
2021-04-26 20:37:27 +0300

    added clinic

---
+++ mentcare/Clinic/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Clinic_profile(models.Model):
+     id=models.UUIDField( primary_key = True, editable = False)
+     name=models.CharField(max_length=100)
+     email=models.CharField(max_length=100)
+     phone=models.CharField(max_length=100)
+
+     class Meta:
+         db_table='Clinic'
+
+
+class Holidays(models.Model):
+    date=models.CharField(max_length=50)
+    hos_id=models.CharField(max_length=70)
+    class Meta:
+        db_table='Holidays'
+
+class Weekends(models.Model):
+    weekday=models.CharField(max_length=50)
+    hos_id=models.CharField(max_length=70)
+    class Meta:
+        db_table='Weekends'
---
+++ mentcare/Clinic/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from user.views import logout
+
+urlpatterns = [
+    path('editClinic/<uuid:id>/', views.Clinic_edit),
+    path('changePassword/<uuid:id>/', views.changePassword),
+    path('clinic/<uuid:id>/', views.Clinic_profile),
+    path('waitingApp/<uuid:hos_id>/', views.waiting_app),
+    path('count/<uuid:hos_id>/', views.count),
+    path('listClinic/',views.listClinic),
+    path('confirm_appointments_now/<uuid:appointment_id>/', views.confirm_appointments_now),
+    path('complete_appointments_now/<uuid:appointment_id>/', views.complete_appointments_now),
+    path('reject_appointments_now/<uuid:appointment_id>/', views.reject_appointments_now),
+    path('addHoliday/<uuid:id>/',views.addHoliday),
+]
