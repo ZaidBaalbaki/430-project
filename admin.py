8a465481fcc43d87450fa869de9d5059aae7f160
ZaidBaalbaki <62421514+ZaidBaalbaki@users.noreply.github.com>
2021-04-26 23:48:12 +0300

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.admin import SimpleListFilter
-
-
-admin.site.register(CustomUser)
